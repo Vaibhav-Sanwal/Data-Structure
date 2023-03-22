b057e05176f428b7997b6095a4daff26c3992614
Vaibhav-Sanwal <109754709+Vaibhav-Sanwal@users.noreply.github.com>
2023-03-22 18:35:07 +0530

    Add files via upload

---
+++ pivot_element.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int getPivot(int arr[], int n){
+	int start=0;
+	int end=n-1;
+	int mid= start+(end-start)/2;
+	
+	while(start<end){
+		if(arr[mid]>=arr[0]){
+			start=mid+1;
+		}
+		else{
+			start=mid;
+		}
+		mid=start+(end-start)/2;
+	}
+	return start;
+}
+
+int main(){
+	int arr[5]={3,8,10,17,1};
+	
+	cout<<"the pivot element is:"<<getPivot(arr,5);
+}
